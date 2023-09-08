fa3bd98cf668009612f41c04a7f56f649ee02b86
jckpn <jacp3@outlook.com>
2023-09-08 13:40:34 +0100

    .

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ $(function() {
         window.location.href = 'mailto:jackpa' + 'ine' + '3@gm' + 'ail.com';
     });
     
-    $('.about-link').click(() => { $('body').addClass('show-about'); });
-    $('.about-back').click(() => { $('body').removeClass('show-about'); });
+    $('.about-link').click(() => { 
+        // look like new url
+        window.location.href = '#about';
+        $('body').addClass('show-about');
+    });
+    $('.about-back').click(() => {
+        window.location.href = '#';
+        $('body').removeClass('show-about');
+    });
 });
