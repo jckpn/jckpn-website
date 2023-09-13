709384893db3f959afccd908e5e47f0a13058aac
jckpn <jacp3@outlook.com>
2023-09-13 12:55:00 +0100

    .

--- main.js
+++ main.js
@@ -4,11 +4,18 @@ $(function() {
     $('.email-addr').on('click', function() {
         window.location.href = 'mailto:jackpa' + 'ine' + '3@gm' + 'ail.com';
     });
-    
+
+    // if url has #about, show about
+    if (window.location.href.indexOf('#about') > -1) {
+        $('body').addClass('show-about');
+    }
+        
     $('.about-link').click(() => { 
         // look like new url
         window.location.href = '#about';
-        $('body').addClass('show-about');
+        setTimeout(() => {
+            $('body').addClass('show-about');
+        }, 100); // brief 'loading' feel
     });
     $('.about-back').click(() => {
         window.location.href = '#';
