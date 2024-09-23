44762531276607d468e4955929c85d2b3da989a4
Jack Paine <jacp3@outlook.com>
2024-09-23 13:51:18 +0100

    .

--- js/background.js
+++ js/background.js
@@ -1,11 +1,4 @@
 $(() => {
-    window.addEventListener('pageshow', function(e) {
-        if (e.persisted) {
-            // reload if page is loaded from cache (e.g. user clicked back)
-            window.location.reload();
-        }
-    });
-
     updateBG(0, 0);
 
     // on move mouse
@@ -17,30 +10,27 @@ $(() => {
         hoveringLink = $(e.target).closest('.links-container').length > 0;
 
         // get mouse x and y as % of window from center
-        mouseX = e.pageX / $(window).width() * 100 - 50;
-        mouseY = e.pageY / $(window).height() * 100 - 50;
+        mouseX = e.pageX / $(window).width() * 200 - 100;
+        mouseY = e.pageY / $(window).height() * 200 - 100;
 
         updateBG(mouseX, mouseY);
     });
 
     function updateBG(mouseX, mouseY) {
-        x = mouseX * 0.05;
-        y = mouseY * 0.08;
-
+        var x = mouseX * 0.02;
+        var y = mouseY * 0.05;
         const d1 = 'M -5,105 L -5,' + (70 + x + y)
             + ' C 40,100'
             + ' ' + (40 + x) + ',' + (50 - x + y)
             + ' 105,105 L';
-        
-        x = mouseX * 0.12;
-        y = mouseY * 0.12;
+        $('#waves-path-1').attr('d', d1);
 
+        var x = mouseX * 0.06;
+        var y = mouseY * 0.06;
         const d2 = 'M -5,105 L -5,' + (55 + x + y)
             + ' C 60,120'
             + ' ' + (40 + x) + ',' + (50 - x + y)
             + ' 105,95 L';
-
-        $('#waves-path-1').attr('d', d1);
         $('#waves-path-2').attr('d', d2);
     }
 });
