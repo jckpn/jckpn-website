56076168a9fec04caa6cb3103cf7904c94acc840
Jack Paine <jacp3@outlook.com>
2024-09-26 14:56:47 +0100

    .

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ $(function () {
         var mouseX = event.pageX / $(window).width() - 0.5;
         var mouseY = event.pageY / $(window).height() - 0.5;
 
-        var sensitivity = 3;
+        var sensitivity = 10;
 
         var rotateX = -mouseY * sensitivity;
         var rotateY = mouseX * sensitivity;
